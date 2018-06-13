0cdde5b402eb6f2e73f2891d54d6471fa9c63f37
Samuel Butta <samuel@butta.cz>
2018-06-13 22:46:07 +0200

    Sanitize edges

--- src/Game.hs
+++ src/Game.hs
@@ -3,13 +3,21 @@ module Game where
 import Model
 
 moveShipLeft :: Model -> Model
-moveShipLeft m@(Started ship score) = m { ship = ((fst ship) + 1, (snd ship)), score = score }
+moveShipLeft m@(Started ship score) = m { ship = (sanitizeEdges (fst ship) 1, (snd ship)), score = score }
 
 moveShipRight :: Model -> Model
-moveShipRight  m@(Started ship score) = m { ship = ((fst ship) - 1, (snd ship)), score = score }
+moveShipRight  m@(Started ship score) = m { ship = (sanitizeEdges (fst ship) (-1), (snd ship)), score = score }
 
 moveShipUp :: Model -> Model
-moveShipUp m@(Started ship score) = m { ship = ((fst ship), (snd ship) - 1), score = score }
+moveShipUp m@(Started ship score) = m { ship = ((fst ship), sanitizeEdges (snd ship) (-1)), score = score }
 
 moveShipDown :: Model -> Model
-moveShipDown m@(Started ship score) = m { ship = ((fst ship), (snd ship) + 1), score = score }
+moveShipDown m@(Started ship score) = m { ship = ((fst ship), sanitizeEdges (snd ship) 1), score = score }
+
+
+sanitizeEdges :: Integer -> Integer -> Integer
+sanitizeEdges coord delta
+  | newCoord < 0 = coord
+  | newCoord > (screenSize - shipSize) = coord
+  | otherwise = newCoord
+    where newCoord = coord + delta
