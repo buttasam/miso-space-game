3d6b6235ce204aaf3c0ade6be52db743656b757b
Samuel Butta <samuel@butta.cz>
2018-06-13 22:22:51 +0200

    Separate move login to Game module

--- src/Game.hs
+++ src/Game.hs
@@ -1,4 +1,17 @@
 module Game where
 
-showText :: String
-showText = "module text"
+import Model
+
+screenSize = 600
+
+moveShipLeft :: Model -> Model
+moveShipLeft m@(Started ship score) = m { ship = ((fst ship) + 1, (snd ship)), score = score }
+
+moveShipRight :: Model -> Model
+moveShipRight  m@(Started ship score) = m { ship = ((fst ship) - 1, (snd ship)), score = score }
+
+moveShipUp :: Model -> Model
+moveShipUp m@(Started ship score) = m { ship = ((fst ship), (snd ship) - 1), score = score }
+
+moveShipDown :: Model -> Model
+moveShipDown m@(Started ship score) = m { ship = ((fst ship), (snd ship) + 1), score = score }
--- src/Update.hs
+++ src/Update.hs
@@ -12,6 +12,7 @@ import Miso.String
 
 import Action
 import Model
+import Game
 
 -- | Updates model, optionally introduces side effects
 updateModel :: Action -> Model -> Effect Action Model
@@ -21,10 +22,10 @@ updateModel InitAction m = m <# do
 updateModel (Tick _) m@(Started ship score) =
   noEff $ m {ship = ship, score = score + 1}
 updateModel (KeyboardPress keys) m@(Started ship score)
-  | Set.member 39 keys = noEff $ m { ship = ((fst ship) + 1, (snd ship)), score = score }
-  | Set.member 37 keys = noEff $ m { ship = ((fst ship) - 1, (snd ship)), score = score }
-  | Set.member 40 keys = noEff $ m { ship = ((fst ship), (snd ship) + 1), score = score }
-  | Set.member 38 keys = noEff $ m { ship = ((fst ship), (snd ship) - 1), score = score }
+  | Set.member 39 keys = noEff $ (moveShipLeft m)
+  | Set.member 37 keys = noEff $ (moveShipRight m)
+  | Set.member 40 keys = noEff $ (moveShipDown m)
+  | Set.member 38 keys = noEff $ (moveShipUp m)
   | Set.member 32 keys = m <# do
       putStrLn "Shoot" >> pure NoOp
   | otherwise = noEff $ m
