93058164d5f78cdd3f79ffac036d789d788825bc
Samuel Butta <samuel@butta.cz>
2018-06-13 19:57:35 +0200

    Add keys handlers

--- src/Update.hs
+++ src/Update.hs
@@ -20,5 +20,11 @@ updateModel InitAction m = m <# do
   putStrLn "Test" >> pure NoOp
 updateModel (Tick _) m@(Started ship score) =
   noEff $ m {ship = ship, score = score + 1}
-updateModel (KeyboardPress keys) m@(Started ship score) =
-  noEff $ m { ship = ((fst ship) + 1, (snd ship)), score = score}
+updateModel (KeyboardPress keys) m@(Started ship score)
+  | Set.member 39 keys = noEff $ m { ship = ((fst ship) + 1, (snd ship)), score = score }
+  | Set.member 37 keys = noEff $ m { ship = ((fst ship) - 1, (snd ship)), score = score }
+  | Set.member 40 keys = noEff $ m { ship = ((fst ship), (snd ship) + 1), score = score }
+  | Set.member 38 keys = noEff $ m { ship = ((fst ship), (snd ship) - 1), score = score }
+  | Set.member 32 keys = m <# do
+      putStrLn "Shoot" >> pure NoOp
+  | otherwise = noEff $ m
