66ee24b3af740cafff46600ed0f2ef14cbd62e55
Samuel Butta <samuel@butta.cz>
2018-06-14 13:12:56 +0200

    Clean up

--- src/Update.hs
+++ src/Update.hs
@@ -18,13 +18,13 @@ import Game
 updateModel :: Action -> Model -> Effect Action Model
 updateModel NoOp m = noEff m
 updateModel InitAction m = m <# do
-  putStrLn "Test" >> pure NoOp
+  putStrLn "Init" >> pure NoOp
 updateModel (Tick _) m =
   noEff $ updateGame m
 updateModel (KeyboardPress keys) m@Started{..}
-  | Set.member 39 keys = noEff $ (moveShipLeft m)
-  | Set.member 37 keys = noEff $ (moveShipRight m)
-  | Set.member 40 keys = noEff $ (moveShipDown m)
-  | Set.member 38 keys = noEff $ (moveShipUp m)
+  | Set.member 39 keys = noEff $ moveShipLeft m
+  | Set.member 37 keys = noEff $ moveShipRight m
+  | Set.member 40 keys = noEff $ moveShipDown m
+  | Set.member 38 keys = noEff $ moveShipUp m
   | Set.member 32 keys = noEff $ shoot m
   | otherwise = noEff $ m
