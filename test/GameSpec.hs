b7a3e176539e8157f31b8bcabb8dd087fba22e1b
Samuel Butta <samuel@butta.cz>
2018-06-15 11:24:28 +0200

    Configure tests, add sanitize edges test

--- test/GameSpec.hs
+++ test/GameSpec.hs
@@ -1,10 +1,14 @@
 import Test.Hspec
 
+import Game
+
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
 spec = do
-  describe "#Basic test" $
-    it "dummy test" $ do
-      1 `shouldBe` 1
+  describe "sanitize edges" $ do
+    it "returns value in ege" $ do
+      (sanitizeEdges 10 5) `shouldBe` 15
+    it "returns value in ege" $
+      (sanitizeEdges 0 (-5)) `shouldBe` 0
